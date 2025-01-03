806febda38cfe14a6d1c754255a603badf1a882e
İsmail Can Varlı <ismailcanvarli@gmail.com>
2025-01-03 22:43:37 +0300

    feat: add screen templates

---
+++ app/src/main/java/com/ismailcanvarli/moviebank/ui/screen/HomeScreen.kt
@@ -0,0 +1,15 @@
+//Created by canVarli on 1/2/2025
+
+package com.ismailcanvarli.moviebank.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.ismailcanvarli.moviebank.viewmodel.HomeViewModel
+
+
+@Composable
+fun HomeScreen(
+    navController: NavController, homeViewModel: HomeViewModel
+) {
+
+}
---
+++ app/src/main/java/com/ismailcanvarli/moviebank/ui/screen/MovieCartScreen.kt
@@ -0,0 +1,10 @@
+//Created by canVarli on 1/3/2025
+
+package com.ismailcanvarli.moviebank.ui.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MovieCartScreen() {
+
+}
